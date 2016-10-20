2de168b2aa968b6a70ac66af436668bd715beb97
Илья <wiks30@gmail.com>
2016-10-20 14:13:35 +0300

    add

---
+++ Task1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Class1
+    {
+        static int[] CreateArray(int lenth)
+        {
+            Random rdm = new Random();
+
+            int[] mass = new int[lenth];
+
+            for (var i = 0; i < lenth; i++)
+            {
+                mass[i] = rdm.Next(int.MinValue, int.MaxValue);
+            }
+
+            Array.Sort(mass);
+
+            return mass;
+        }
+
+        static int? BinarySearch(int[] array, int number)
+        {
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+
+            int low = 0;
+            int high = array.Length;
+            int mid = (low + high) / 2;
+
+            watch.Start();
+            while (low < high)
+            {
+                mid = (low + high) / 2;
+                if (number == array[mid])
+                    return mid;
+                if (number > array[mid])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            watch.Stop();
+
+            return null;
+        }
+    }
+}
